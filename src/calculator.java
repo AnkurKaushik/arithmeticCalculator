6bfde3a49c539497b750cab0f0502d4b994cac13
AnkurKaushik <a123ankur@yahoo.com>
2021-01-27 16:01:54 -0600

    started creating the calculator

---
+++ src/calculator.java
@@ -0,0 +1,32 @@
+public class calculator {
+    public void add(){
+    }
+    public void subtract(){
+    }
+    public void multiply(){
+    }
+    public void divide(){
+    }
+
+    public static void main(String[] args){
+        boolean running = true;
+        while(running)
+        {
+            String userInput = "";
+            switch(userInput)
+            {
+                case "add":
+                    break;
+                case "subtract":
+                    break;
+                case "multiply":
+                    break;
+                case "divide":
+                    break;
+                case "quit":
+                    running = false;
+                    break;
+            }
+        }
+    }
+}
