db110398f47eb9ca6adcaeb243b852d2c454bb8d
AnkurKaushik <a123ankur@yahoo.com>
2021-01-28 16:03:17 -0600

    adding try catch loop to handle incorrect inputs

--- src/calculator.java
+++ src/calculator.java
@@ -1,7 +1,10 @@
 import java.sql.SQLOutput;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class calculator {
+
+    //the next 4 functions contain the functionality of the arithmetic calculations, they are called by the switch statement branches
     private static double add(double f, double s){
         return f+s;
     }
@@ -15,25 +18,35 @@ public class calculator {
         return f/s;
     }
 
+    //this prompt asks the user what numbers they want to perform operations on and stores them in an aarry of doubles
     private static double[] prompt(Scanner s)
     {
-        System.out.println("Please input your first number");
-        double first = s.nextDouble();
-        System.out.println("Please input your second number");
-        double second = s.nextDouble();
+        try
+        {
+            System.out.println("Please input your first number");
+            double first = s.nextDouble();
+            System.out.println("Please input your second number");
+            double second = s.nextDouble();
 
-        return new double[]{first, second};
+            return new double[]{first, second};
+        } catch (InputMismatchException e)
+        {
+            System.out.println("You entered an invalid number");
+        };
+        return null;
     }
 
     public static void main(String[] args){
-        boolean running = true;
+        boolean running = true; //remains true until user types "quit"
 
         Scanner s;
         while(running)
         {
             System.out.println("Please enter the operation you would like to perform (add, subtract, multiply, divide) \nType 'quit' to quit");
             s = new Scanner(System.in);
-            String userInput = s.nextLine().toLowerCase();
+            String userInput = s.nextLine().toLowerCase(); // sets to lower to account for lowercase
+
+            //switch statement has branches for the 4 operations and quit, if the input is something else they will be asked to enter a valid input
             switch(userInput)
             {
                 case "add":
